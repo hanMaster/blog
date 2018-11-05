5ed110981c075d0f73c78eff017b4487fe387e8a
hanMaster <w54661c@gmail.com>
2018-11-05 23:44:06 +1000

    Admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function(){
+    Route::get('/', 'DashboardController@dashboard');
+});
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
